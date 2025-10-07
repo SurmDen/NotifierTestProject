98c43a09c9024e0e6903ace1ae0f3f98fdb7f161
SurmDen <surmanidzedenis609@gmail.com>
2025-10-07 20:46:29 +0300

    fail

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,22 +5,14 @@ namespace NotifierTestProject.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(IConfiguration configuration)
+        public ApplicationDbContext()
         {
-            _configuration = configuration;
             Database.EnsureCreated();
         }
-
-        private readonly IConfiguration _configuration;
 
         public DbSet<User> Users { get; set; }
 
         public DbSet<Notice> Notices { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite(_configuration.GetConnectionString("SQLiteConnection") ?? "notice_app.db");
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
